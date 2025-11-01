8d0706cdc23f994b27042458b8d0f1d9ab1f6ddf
Shinnosuke Kawaguchi <kawaguchi.shin.03@gmail.com>
2025-11-01 10:49:10 +0900

    enable by default.

--- src/app/_components/animation-switch.tsx
+++ src/app/_components/animation-switch.tsx
@@ -10,6 +10,9 @@ export default function AnimationSwitch() {
 
   useEffect(() => {
     setIsMounted(true);
+    if (isA11yEnabled === undefined) {
+      setIsA11yEnabled(true);
+    }
   }, []);
 
   if (!isMounted) {
